5971883d128ab55b448aa159ad474a1428e9f117
Vinayak Raj Ranjan <96948435+vinayakjaas@users.noreply.github.com>
2023-12-18 18:03:17 +0530

    Delete gatway_Integrate_CI_CD_Jenkins directory

--- gatway_Integrate_CI_CD_Jenkins/defaultFiles/javascript.js
+++
@@ -1,9 +0,0 @@
-// INSTRUCTIONS:
-
-// Use the predifined function name with the required parameters
-// Return the specified value from the function
-// Program execution must stop before the maximum time limit
-// Do not include the main function's call in code
-
-
-// Write function code from here
